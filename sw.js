d1ad275f029f557b8a82e2e83b5de02cad22c5c2
Alberto Jonas Sanchez Mejias <jonaswtf2003@gmail.com>
2026-09-06 17:42:43 -0400

    Update service worker cache name to v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pastichos-bunker-v18-1';
+const CACHE_NAME = 'pastichos-bunker-v19';
 const urlsToCache = [
   './',
   './index.html',
@@ -8,7 +8,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // ÓRDEN NUEVA: El nuevo vigía toma el control de inmediato, sin esperar.
+  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
@@ -38,5 +38,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  self.clients.claim(); // ÓRDEN NUEVA: Aplica la nueva versión a todas las pantallas abiertas.
+  self.clients.claim(); 
 });
